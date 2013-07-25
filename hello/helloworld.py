ffe5bbb770e146da3a05fadff0be37ac68e8d55c
lpshou <843245092@qq.com>
2013-07-25 20:29:04 +0800

    first commit

---
+++ hello/helloworld.py
@@ -0,0 +1,14 @@
+'''
+Created on 2013-7-24
+
+@author: lpshou.liu
+'''
+#one comment
+#userName = raw_input('ENTER YOUR NAME:')
+#filename = raw_input('enter file name:')
+#!/usr/bin/env python
+str1 = 'hello world'
+str2 = 134
+print id(str2)
+print
+print type(str2)
